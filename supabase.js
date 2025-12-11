02bb73df4f1caab99e61c363e894fd5959049814
ChristineMorate14 <lnrdjjmarie@gmail.com>
2025-12-11 14:26:46 +0800

    Remove sensitive information from supabase.js
    Removed project URL and anon key from supabase.js

--- supabase.js
+++ supabase.js
@@ -1,6 +1,3 @@
-// Replace with your project URL and anon key
 const SUPABASE_URL = "https://iosezfjvavmrlakobzej.supabase.co";
 const SUPABASE_ANON_KEY = "sb_publishable_v023oj_IxpqsRQ7sYPuhkA_EmWJJ22B";
-
-// Initialize Supabase client
 const supabase = Supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
